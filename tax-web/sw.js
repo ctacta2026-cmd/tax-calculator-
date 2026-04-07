4521f7829650a734cef0258b82575ee31a65a54d
ctacta2026-cmd <ctacta2026@gmail.com>
2026-04-07 15:23:51 +0900

    Add files via upload

---
+++ tax-web/sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'tax-sim-v2';
+const urlsToCache = [
+  'index.html',
+  'icon-192.png',
+  'icon-512.png',
+  'manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
